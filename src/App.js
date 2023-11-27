10c533551c5ce790e71d789996f71db7b27a8d1a
SughoshBhargav <sughoshasv.cs20@sahyadri.edu.in>
2023-11-27 18:47:34 +0530

    created header components

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import SignIn from "./pages/SignIn.jsx"
 import SignUp from "./pages/SignUp.jsx"
 import ForgotPassword from "./pages/ForgotPassword.jsx"
 import Offers from "./pages/Offers.jsx"
+import Header from "./components/Header.jsx";
 
 
 function App() {
   return (
     <>
     <Router>
+      <Header/>
       <Routes>
         <Route path = "/" element={<Home/>}></Route>
         <Route path = "/profile" element={<Profile/>}></Route>
