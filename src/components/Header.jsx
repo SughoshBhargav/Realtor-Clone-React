eba2b6a28bd6ac18bd013023be39419299a79889
SughoshBhargav <sughoshasv.cs20@sahyadri.edu.in>
2023-12-02 20:06:34 +0530

    created spinner componenet

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,7 +32,7 @@ export default function Header() {
 
 
   return (
-    <div className='bg-white border-b shadow-sm sticky top-0 z-50'>
+    <div className='bg-white border-b shadow-sm sticky top-0 z-40'>
         <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
             <div>
                 <img src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg" alt="logo" className='h-5 cursor-pointer' onClick={()=>navigate("/")}/>
@@ -41,7 +41,7 @@ export default function Header() {
                 <ul className='flex space-x-10'>
                     <li className={`cursor-pointer py-3 text-sm font-semibold ${pathMatchRoute("/") ? 'text-black border-b-[3px] border-b-red-500' : 'text-gray-400 border-b-[3px] border-b-transparent'}`} onClick={()=>navigate("/")}>Home</li>
                     <li className={`cursor-pointer py-3 text-sm font-semibold ${pathMatchRoute("/offers") ? 'text-black border-b-[3px] border-b-red-500' : 'text-gray-400 border-b-[3px] border-b-transparent'}`} onClick={()=>navigate("/offers")}>Offers</li>
-                    <li className={`cursor-pointer py-3 text-sm font-semibold ${pathMatchRoute("/sign-in") || pathMatchRoute("/sign-in")  ? 'text-black border-b-[3px] border-b-red-500' : 'text-gray-400 border-b-[3px] border-b-transparent'}`} onClick={() => navigate("/profile")}>{pageState}</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold ${pathMatchRoute("/sign-in") || pathMatchRoute("/profile")  ? 'text-black border-b-[3px] border-b-red-500' : 'text-gray-400 border-b-[3px] border-b-transparent'}`} onClick={() => navigate("/profile")}>{pageState}</li>
                     
 
                 </ul>
--- src/hooks/useAuthStatus.jsx
+++ src/hooks/useAuthStatus.jsx
@@ -1,20 +1,26 @@
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 export function useAuthStatus() {
-  
-    const [loggedIn,setLoggedIn] = useState(false);
-    const [checkingStatus,setCheckingStatus] = useState(true);
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [checkingStatus, setCheckingStatus] = useState(true);
 
-    useEffect(()=>{
-        const auth = getAuth()
-        onAuthStateChanged(auth,(user)=>{
-            if(user){
-                setLoggedIn(true)
+    useEffect(() => {
+        const auth = getAuth();
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setLoggedIn(true);
+            } else {
+                setLoggedIn(false); // Set loggedIn to false if there's no authenticated user
             }
-            setCheckingStatus(false)
-        })
-    },[])
+            setCheckingStatus(false);
+        });
 
-    return{loggedIn,checkingStatus}
+        // Clean up the listener when the component unmounts
+        return () => {
+            unsubscribe();
+        };
+    }, []);
+
+    return { loggedIn, checkingStatus };
 }
