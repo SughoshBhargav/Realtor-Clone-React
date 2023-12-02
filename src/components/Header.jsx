dd1b43f8cc9f7fb51e10c7520078ba7302bb5522
SughoshBhargav <sughoshasv.cs20@sahyadri.edu.in>
2023-12-02 18:00:24 +0530

    completed header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import React, { useEffect, useState } from 'react'
 import { useLocation,useNavigate } from 'react-router-dom'
 
 export default function Header() {
+
+    const [pageState,setPageState] = useState("Sign in")
     const location = useLocation()
     const navigate = useNavigate()
+    
+    const auth = getAuth()
+    useEffect(()=>{
+        onAuthStateChanged(auth,(user)=>{
+            if(user){
+                setPageState("Profile")
+            }
+            else{
+                setPageState("Sign In")
+            }
+        })
+    },[auth])
+
+
     function pathMatchRoute(route){
         
         if(route === location.pathname){
@@ -24,7 +41,9 @@ export default function Header() {
                 <ul className='flex space-x-10'>
                     <li className={`cursor-pointer py-3 text-sm font-semibold ${pathMatchRoute("/") ? 'text-black border-b-[3px] border-b-red-500' : 'text-gray-400 border-b-[3px] border-b-transparent'}`} onClick={()=>navigate("/")}>Home</li>
                     <li className={`cursor-pointer py-3 text-sm font-semibold ${pathMatchRoute("/offers") ? 'text-black border-b-[3px] border-b-red-500' : 'text-gray-400 border-b-[3px] border-b-transparent'}`} onClick={()=>navigate("/offers")}>Offers</li>
-                    <li className={`cursor-pointer py-3 text-sm font-semibold ${pathMatchRoute("/sign-in") ? 'text-black border-b-[3px] border-b-red-500' : 'text-gray-400 border-b-[3px] border-b-transparent'}`} onClick={() => navigate("/sign-in")}>Sign In</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold ${pathMatchRoute("/sign-in") || pathMatchRoute("/sign-in")  ? 'text-black border-b-[3px] border-b-red-500' : 'text-gray-400 border-b-[3px] border-b-transparent'}`} onClick={() => navigate("/profile")}>{pageState}</li>
+                    
+
                 </ul>
             </div>
         </header>
