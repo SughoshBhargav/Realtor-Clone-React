4a5cd1b975a792fb8eb042968e8b84cc44c891d9
SughoshBhargav <sughoshasv.cs20@sahyadri.edu.in>
2024-04-23 08:13:29 +0530

    created listing page and used swiper to add image slide

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import PrivateRoute from "./components/PrivateRoute.jsx";
 import CreateListing from "./pages/CreateListing.jsx";
 import EditListing from "./pages/EditListing.jsx";
+import Listing from "./pages/Listing.jsx";
 
 
 function App() {
@@ -27,6 +28,7 @@ function App() {
         <Route path = "/sign-up" element={<SignUp/>}></Route>
         <Route path = "/forgot-password" element={<ForgotPassword/>}></Route>
         <Route path = "/offers" element={<Offers/>}></Route>
+        <Route path = "/category/:categoryName/:listingId" element={<Listing/>}></Route>
         <Route path="/create-listing" element = {<PrivateRoute/>}>
           <Route path = "/create-listing" element={<CreateListing/>}></Route>
         </Route>
